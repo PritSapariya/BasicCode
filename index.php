d420ae2393d3888136e62c7d72f29bc539dac1c0
PritSapariya <pritsapariya17@gmail.com>
2020-04-09 17:12:39 +0530

    Second commit for index page

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php include("header.php");?>
 <?php include("menu.php");?>
-<h1>Thi is my First Git Command to commit</h1>
+<h1>This is my First Git Command to commit</h1>
 
 <?php include("footer.php");?>
-    
